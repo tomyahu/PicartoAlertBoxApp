603f0c13a78c26d13b01946a1f5e9fc2bdf1adbf
tomyahu <tomyahu@gmail.com>
2017-12-24 19:50:12 -0300

    Added the base of input manager

---
+++ controller/input/input_manager.py
@@ -0,0 +1,34 @@
+import abc
+import pygame
+
+
+pygame.init()
+
+# Pygame input sources
+mouse = pygame.mouse
+
+
+class InputManager(metaclass=abc.ABCMeta):
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def check_input(self):
+        pass
+
+    @abc.abstractmethod
+    def do_action(self, controller):
+        pass
+
+
+class LeftMouseInputManager(InputManager):
+
+    def __init__(self):
+        InputManager.__init__(self)
+
+    def check_input(self):
+        return mouse.get_pressed()[0]
+
+    def do_action(self, controller):
+        pass
