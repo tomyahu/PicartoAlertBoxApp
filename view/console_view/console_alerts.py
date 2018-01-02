efa5f53c4c1e01633e02672527753df236d055ea
tomyahu <tomyahu@gmail.com>
2018-01-02 18:41:49 -0300

    Modified console alerts because it has nothing to do with tkinter

--- view/console_view/console_alerts.py
+++ view/console_view/console_alerts.py
@@ -1,10 +1,11 @@
 from view.configuration_views.components.canvas_objects.abstract_tkinter_canvas_object import AbstractTkinterCanvasObject
 
 
-class ConsoleAlerts(AbstractTkinterCanvasObject):
+class ConsoleAlerts():
 
     def __init__(self):
-        AbstractTkinterCanvasObject.__init__(self)
+        self.prefix = ''
+        self.suffix = ' just followed!!'
 
     def show(self, name):
-        print(self.prefix_text + name + self.suffix_text)
+        print(self.prefix + name + self.suffix)
