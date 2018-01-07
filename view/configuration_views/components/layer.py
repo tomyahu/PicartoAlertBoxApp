6173fba903c8a7dda6e0443cacdae56b7bf433c1
tomyahu <tomyahu@gmail.com>
2018-01-07 20:14:56 -0300

    Added some methods to layer

--- view/configuration_views/components/layer.py
+++ view/configuration_views/components/layer.py
@@ -1,10 +1,39 @@
 from canvas_objects import *
+from ..tkinter_abstract_obj_options import TkinterAbstractObjectOptions
+import Tkinter as Tk
+
 
 class Layer:
 
-    def __init__(self, configuration_view, root):
-        self.config_view = configuration_view
+    def __init__(self, object_options, root, name):
+        # type: (TkinterAbstractObjectOptions, Tk.Tk, str) -> None
+        self.obj_options = object_options
         self.root = root
+        self.name = name
+        self.selected = False
+        self.obj_options.update()
+
+    def get_object_options(self):
+        return self.obj_options
 
     def show_config_view(self):
-        self.config_view.show()
+        self.obj_options.show()
+
+    def move_object(self):
+        self.obj_options.move_object()
+        self.root
+
+    def get_name(self):
+        return self.name
+
+    def rename(self, new_name):
+        self.name = new_name
+
+    def select(self):
+        self.selected = True
+
+    def unselect(self):
+        self.selected = False
+
+    def is_selected(self):
+        return self.selected
